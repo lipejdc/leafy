f582833eb7523c2f0a80aa86091b16d2defcab70
Jannik Strohbeck <jannikstrohbeck@gmail.com>
2025-07-21 11:31:28 +0200

    connect db

---
+++ pages/api/plants/index.js
@@ -0,0 +1,14 @@
+import dbConnect from "../../../db/connect";
+import Plant from "@/db/models/Plant";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const plants = await Plant.find();
+    console.log(plants);
+    return response.status(200).json(plants);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
+}
