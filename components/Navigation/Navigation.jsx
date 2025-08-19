7cbbff5b1ca14c0f92562f0e428929f1ed3e7383
Felipe <lipejdc@gmail.com>
2025-08-19 11:31:29 +0200

    add plants to list and move sign-in button to the right of navigation

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import { Header, TopNav, NavLink, Title } from "./styles";
+import { Header, TopNav, NavLink, Title, NavLinksGroup } from "./styles";
 import { useSession, signIn, signOut } from "next-auth/react";
 import styled from "styled-components";
 import { LogIn, LogOut } from "lucide-react";
@@ -8,7 +8,7 @@ const AuthButton = styled.button`
   align-items: center;
   gap: 0.5rem;
   padding: 0.5rem 1rem;
-
+  margin-left: auto;
   background: ${(props) => (props.signout ? "#e53935" : "")};
 `;
 
@@ -19,23 +19,27 @@ export default function Navigation() {
   return (
     <Header>
       <TopNav>
-        <NavLink href="/">Home</NavLink>
-        {session && <NavLink href="/myplants">My Plants</NavLink>}
-        <NavLink href="/plant-of-the-day">Plant of the Day</NavLink>
+        <div /> {/* Left placeholder */}
+        <NavLinksGroup>
+          <NavLink href="/">Home</NavLink>
+          {session && <NavLink href="/myplants">My Plants</NavLink>}
+          <NavLink href="/plant-of-the-day">Plant of the Day</NavLink>
+        </NavLinksGroup>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          {!isLoading && !session && (
+            <AuthButton onClick={() => signIn("google")}>
+              <LogIn size={18} /> Sign In
+            </AuthButton>
+          )}
 
-        {!isLoading && !session && (
-          <AuthButton onClick={() => signIn("google")}>
-            <LogIn size={18} /> Sign In
-          </AuthButton>
-        )}
+          {!isLoading && session && (
+            <AuthButton $signout onClick={() => signOut({ callbackUrl: "/" })}>
+              <LogOut size={18} /> Sign Out
+            </AuthButton>
+          )}
 
-        {!isLoading && session && (
-          <AuthButton $signout onClick={() => signOut({ callbackUrl: "/" })}>
-            <LogOut size={18} /> Sign Out
-          </AuthButton>
-        )}
-
-        {isLoading && <p>Checking session...</p>}
+          {isLoading && <p>Checking session...</p>}
+        </div>
       </TopNav>
       <Title>Leafy</Title>
     </Header>
